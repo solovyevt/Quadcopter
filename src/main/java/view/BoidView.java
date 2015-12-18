ebcdbb9f11cd74c700342d3e6b5fbc5b2cc67574
zukokolkka <zukokolkka@user.noreply.github.com>
2015-12-18 21:55:18 +0300

    Fixes

--- src/main/java/view/BoidView.java
+++ src/main/java/view/BoidView.java
@@ -16,7 +16,6 @@ public class BoidView implements Observer {
     public BoidView(){
 
     }
-    
     public void update(Observable o, Object arg) {
         if(arg instanceof Boid){
             render((Boid) arg);
